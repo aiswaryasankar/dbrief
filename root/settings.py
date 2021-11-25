237ab17b1e512feffd4386cdc029be66402aeea3
Aiswarya Sankar <aiswarya.s@uber.com>
2021-11-25 14:26:05 -0800

    Update settings file

--- root/settings.py
+++ root/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if 'RDS_DB_NAME' in os.environ:
+if os.environ and 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
