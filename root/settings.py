e376dbe31815ab3f8fa53c66a0e4fdfc123f2f15
Aiswarya Sankar <aiswarya.s@uber.com>
2021-11-25 23:41:18 -0800

    Add the auth

--- root/settings.py
+++ root/settings.py
@@ -60,9 +60,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-]
+AUTHENTICATION_BACKENDS = (
+    ('django.contrib.auth.backends.ModelBackend'),
+)
 
 ROOT_URLCONF = 'root.urls'
 
