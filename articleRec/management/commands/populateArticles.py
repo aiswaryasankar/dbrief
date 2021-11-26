224aa49c6f6c29afc54414b809242f9b515a5ceb
Aiswarya Sankar <aiswarya.s@uber.com>
2021-11-25 18:02:43 -0800

    Update populateArticles

--- articleRec/management/commands/populateArticles.py
+++ articleRec/management/commands/populateArticles.py
@@ -5,9 +5,4 @@ class Command(BaseCommand):
 
   def handle(self, *args, **options):
 
-    print("Started the cron job")
-    handler = LogtailHandler(source_token="tvoi6AuG8ieLux2PbHqdJSVR")
-    logger = logging.getLogger(__name__)
-    logger.handlers = []
-    logger.addHandler(handler)
-    logger.info('LOGTAIL TEST')
+    self.stdout("Started the cron job")
