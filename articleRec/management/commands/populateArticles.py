ef7ce72d56a8aa55a9286a114443f6652effb683
Aiswarya Sankar <aiswarya.s@uber.com>
2021-12-25 12:13:20 -0800

    Call populate articles from cron

--- articleRec/management/commands/populateArticles.py
+++ articleRec/management/commands/populateArticles.py
@@ -1,6 +1,7 @@
 from django.core.management import BaseCommand
 import logging
 from logtail import LogtailHandler
+from handler import *
 
 handler = LogtailHandler(source_token="tvoi6AuG8ieLux2PbHqdJSVR")
 logger = logging.getLogger(__name__)
@@ -15,3 +16,9 @@ class Command(BaseCommand):
     print("Started cron job")
     logging.info("Started cron job")
     self.stdout.write("Started the cron job", ending='')
+    res = populate_articles_batch()
+    logging.info(res.num_articles_populated)
+
+
+
+
