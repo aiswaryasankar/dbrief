93464d630ef20efaad2a8c95c0181450114867a6
Aiswarya Sankar <aiswarya.s@uber.com>
2021-12-25 11:55:47 -0800

    Update cron schedule and add logging

--- articleRec/management/commands/populateArticles.py
+++ articleRec/management/commands/populateArticles.py
@@ -1,8 +1,17 @@
 from django.core.management import BaseCommand
+import logging
+from logtail import LogtailHandler
 
+handler = LogtailHandler(source_token="tvoi6AuG8ieLux2PbHqdJSVR")
+logger = logging.getLogger(__name__)
+logger.handlers = []
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 class Command(BaseCommand):
 
   def handle(self, *args, **options):
 
+    print("Started cron job")
+    logging.info("Started cron job")
     self.stdout.write("Started the cron job", ending='')
