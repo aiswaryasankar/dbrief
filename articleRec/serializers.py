6db50004f23ace43026b4d3c9b114c8a27bdb050
Aiswarya Sankar <aiswarya.s@uber.com>
2021-12-25 22:19:24 -0800

    enforce JSONParser

--- articleRec/serializers.py
+++ articleRec/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import ArticleModel
 from idl import *
 from rest_framework_dataclasses.serializers import DataclassSerializer
+from rest_framework.parsers import JSONParser
 
 """
   This file will define all the Serializers for the articleRec service.  Serializers serve to map and handle validation between httpRequest objects passed into the service and the dataclasses used to store and pass data around internally between the various apps.
@@ -16,22 +17,27 @@ class ArticleSerializer(serializers.ModelSerializer):
     db_table = "articleModel"
 
 class HelloWorldRequestSerializer(DataclassSerializer):
+  parser_classes = JSONParser
   class Meta:
     dataclass = HelloWorldRequest
 
 class PopulateArticleRequestSerializer(DataclassSerializer):
+  parser_classes = JSONParser
   class Meta:
     dataclass = PopulateArticleRequest
 
 class SaveArticleRequestSerializer(DataclassSerializer):
+  parser_classes = JSONParser
   class Meta:
     dataclass = SaveArticleRequest
 
 class FetchArticlesRequestSerializer(DataclassSerializer):
+  parser_classes = JSONParser
   class Meta:
     dataclass = FetchArticlesRequest
 
 class HydrateArticleRequestSerializer(DataclassSerializer):
+  parser_classes = JSONParser
   class Meta:
     dataclass = HydrateArticleRequest
 
