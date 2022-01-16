987cdc67d352fedc2795e9b01d53935fd27958e7
Aiswarya Sankar <aiswarya.s@uber.com>
2022-01-15 23:18:57 -0800

    Update CORS for django

--- root/settings.py
+++ root/settings.py
@@ -49,6 +49,7 @@ LOGGING = {
 
 # Application definition
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,6 +70,12 @@ INSTALLED_APPS = [
     'logtail',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://debrief-ai.web.app/",
+    'http://localhost:3001/',
+    ""
+]
+
 CRONJOBS = [
     ('*/5 * * * *', 'articleRec.management.commands.populateArticles.py ')
 ]
@@ -83,6 +90,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
