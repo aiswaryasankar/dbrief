84a5b2b4aa9b123ef57b9ce611206eb49f65dd0f
Aiswarya Sankar <aiswarya.s@uber.com>
2022-03-05 21:32:18 -0800

    Add firebaseUserId to tables

--- userPreferences/models.py
+++ userPreferences/models.py
@@ -28,6 +28,7 @@ class UserTopicModel (models.Model):
   userTopicId = models.AutoField(primary_key=True)
   userId = models.IntegerField("UserId", null=True)
   topicId = models.IntegerField("TopicId", null=True)
+  firebaseAuthId = models.TextField("FirebaseAuthId", null=True)
   forNewsletter = models.BooleanField("For Newsletter", default=False)
 
   def __str__(self):
