97547b2e3849d37a815252c5ce45e18b5b7a7186
Aiswarya Sankar <aiswarya.s@uber.com>
2022-08-21 16:06:37 -0700

    Update readme and newsletter test

--- newsletter/tests/test_views.py
+++ newsletter/tests/test_views.py
@@ -1,6 +1,33 @@
 from django.test import TestCase
+from django import test
+from django.test import Client
 
-# Create your tests here.
+
+"""
+  This file will handle checking that all the endpoints are properly called and have a 200 response code.  More advanced functionality can be tested in the future.
+"""
+
+
+class NewsletterViewTest(TestCase):
+
+  def test_create_newsletter(self):
+
+    response = self.client.post('/createNewsletterConfig/',
+			data={
+				"newsletterConfig": {
+					"NewsletterConfigId": 1,
+					"UserID": 1,
+					"DeliveryTime": "MORNING",
+					"RecurrenceType": "DAILY",
+					"IsEnabled": True,
+					"TopicsFollowed": [1]
+				}
+			}
+		, content_type="application/json")
+    self.assertIsNotNone(response.content)
+    self.assertEqual(response.status_code, 200)
+
+
 """
 Create_newsletter_config_for_user
 
