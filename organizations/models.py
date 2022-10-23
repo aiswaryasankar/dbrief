8cae2632b48e570ba073ec3a35c9b68790f73318
Aiswarya Sankar <aiswarya.s@uber.com>
2022-10-22 22:30:07 -0700

    Add location to createOrganization

--- organizations/models.py
+++ organizations/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 
 """
-  All data models for the organization service including the Organization and UserOrgAssociation
+  All data models for the organization service including the Organization, Location and UserOrgAssociation
 """
 
 class OrganizationModel (models.Model):
@@ -21,3 +21,24 @@ class OrganizationModel (models.Model):
 
   def __str__(self):
     return self.uuid
+
+
+
+class LocationModel (models.Model):
+
+  id = models.AutoField(primary_key=True)
+  uuid = models.CharField("UUID", unique=True, max_length=36)
+  street = models.TextField("Street", null=True)
+  city = models.TextField("City", null=True)
+  state = models.TextField("State", null=True)
+  zip = models.TextField("Zip", null=True)
+  country = models.TextField("Country", null=True)
+
+  class Meta:
+    app_label = "organizations"
+
+  def __str__(self):
+    return self.street + " " + self.city + ", " + self.state
+
+
+
